4ed427a679694b78ee6ba2d10fc760b525b7105c
SandeepJoelFreshdesk <sandeepjoel@freshdesk.com>
2018-09-11 15:39:48 +0530

    Add full logs

--- shell-scripts/install.sh
+++ shell-scripts/install.sh
@@ -1,20 +1,15 @@
 export install_variable="this is from install"
 echo "Installing ack-grep"
 sudo apt-get install ack-grep
-sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 echo "[0]       create  build/content/en-US/freshdesk/scaling-support/agent-collision-detection/index.html.redirect
 [0]       create  build/content/en-US/freshdesk/scaling-support/mobile-help-desk-management/index.html.redirect
 [0]       create  build/content/en-US/freshdesk/multi-lingual-global-helpdesk/index.html.redirect
 [0]       create  build/content/en-US/freshdesk/freshchat/index.html.redirect
 [0]       create  build/content/en-US/freshdesk/multichannel-support/phone-support/after-call-work/index.html.redirect
-[0]       create  build/content/en-US/freshdesk/multichannel-support/phone-support/buy-phone-number/index.html.redirect
-[0]       create  build/content/en-US/freshdesk/multichannel-support/phone-support/call-center-helpdesk/index.html.redirect
-[0]       create  build/content/en-US/freshdesk/multichannel-support/phone-support/call-conferencing/index.html.redirect
-[0]       create  build/content/en-US/freshdesk/multichannel-support/phone-support/call-lifetime-metric
-[0]       create  build/content/en-US/freshdesk/multichannel-support/phone-support/call-lifetime-metric
-[0]       create  build/content/en-US/collaboration-software/signup/index.html
-[0]        error  build/content/en-US/404.html
-[0]       create  build/content/en-US/sitemap.xml
+[0]       create  build/assets/images/en-US/ada.png
 " > log.txt
 echo "Testing ack-grep"
-cat log.txt || ack 'create'
+cat log.txt | ack 'error'
+echo "Testing ack-grep full regex"
+cat log.txt | ack '==|\b(create|update|error|delete)\b\s+(?!build\/(assets\/)?(images|thumbnails)|\s)'
+

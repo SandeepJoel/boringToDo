5781873cfa88378b1d6d5459302d86d99a8c8a4e
SandeepJoel <joeltheking00000001@gmail.com>
2020-02-24 16:20:37 +0530

    Add untested cloud function
    Need to work on it later

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+
+var serviceAccount = require("./boringtodo-1445e-firebase-adminsdk-xgujm-b41cb77b9a.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://boringtodo-1445e.firebaseio.com"
+});
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,79 +19,132 @@ admin.initializeApp(functions.config().firebase);
   Also think and consult about how to implement resetting 
   to default values in settings 
 */
+
+const defaultSettings = {
+  general: {
+    theme: {
+      options: ['light', 'dark'],
+      current: 'light'
+    },
+    layout: {
+      options: ['left', 'right', 'blank', 'leftRight', 'center'],
+      current: 'blank'
+    },
+    widgets: ['todo']
+  },
+  backgroundEffects: [
+    {
+      type: 'plainBackground',
+      color: "#FFF",
+      activated: true
+    },
+    {
+      type: 'colorLiquids',
+      activated: false,
+      config: [
+        {
+          type: 'gradient',
+          colors: ['#4facfe', '#00f2fe'],
+          wave: 'blob1'
+        },
+        {
+          type: 'singleColor',
+          color: '#000',
+          wave: 'blob1'
+        },
+        {
+          type: 'gradient',
+          colors: ['#f093fb', '#f5576c'],
+          wave: 'blob2'
+        },
+        {
+          type: 'singleColor',
+          color: '#FFF',
+          wave: 'blob2'
+        },
+        {
+          type: 'gradient',
+          colors: ['#84fab0', '#8fd3f4'],
+          wave: 'blob1'
+        },
+        {
+          type: 'singleColor',
+          color: '#000',
+          wave: 'blob1'
+        },
+        {
+          type: 'gradient',
+          colors: ['#fa709a', '#fee140'],
+          wave: 'blob1'
+        },
+      ],
+    }
+  ],
+}
+
+
+
 exports.createNewUserDbsetup = functions.auth.user().onCreate((user) => {
+  // Assuming first element in `backgroundEffects` is active by default
+  let ActiveBackgroundEffect = JSON.parse(JSON.stringify(defaultSettings.backgroundEffects[0]));
+  delete ActiveBackgroundEffect.activated;
+
   admin.firestore().collection('users').add({
     userDetails: {
       name: user.displayName,
       email: user.email
     },
     settings: {
-      general: {
-        theme: {
-          options: ['light', 'dark'],
-          current: 'light'
-        },
-        layout: {
-          options: ['left', 'right', 'blank', 'leftRight', 'center'],
-          current: 'blank'
-        },
-        widgets: ['todo']
-      },
-      backgroundEffects: [
-        {
-          type: 'plain-background',
-          color: "#FFF",
-          activated: true
-        },
-        {
-          type: 'color-liquids',
-          activated: false,
-          config: [
-            {
-              type: 'gradient',
-              colors: ['#4facfe', '#00f2fe'],
-              wave: 'blob1'
-            },
-            {
-              type: 'singleColor',
-              color: '#000',
-              wave: 'blob1'
-            },
-            {
-              type: 'gradient',
-              colors: ['#f093fb', '#f5576c'],
-              wave: 'blob2'
-            },
-            {
-              type: 'singleColor',
-              color: '#FFF',
-              wave: 'blob2'
-            },
-            {
-              type: 'gradient',
-              colors: ['#84fab0', '#8fd3f4'],
-              wave: 'blob1'
-            },
-            {
-              type: 'singleColor',
-              color: '#000',
-              wave: 'blob1'
-            },
-            {
-              type: 'gradient',
-              colors: ['#fa709a', '#fee140'],
-              wave: 'blob1'
-            },
-          ],
-        }
-      ],
+      general: defaultSettings.general,
+      activeBackgroundEffect: ActiveBackgroundEffect,
     }
   })
-  .then((docRef) => {
-    return console.log("Document written with ID: ", docRef.id)
-    //admin.firestore().collection('users').doc(docRef.id)
+  .then((docRef) => { 
+    console.log("User document written with ID: ", docRef.id);    
+    var batch = admin.firestore().batch();
+    defaultSettings.backgroundEffects.forEach((setting) => {
+      let ref = admin.firestore()
+                    .collection(`/users/${docRef.id}/backgroundEffectsCollection`)
+                    .doc(setting.type);
+      batch.set(ref, setting);
+    });
+    return batch.commit();
+  }).
+  then((val) => { 
+    console.log("Final value: ", val);    
+    return val;
   })
   .catch((error) => {
     console.error("Error adding document: ", error);
   });
 });
+
+/* 
+  Need to check why this is not running in emulator ?
+  Throwed error 4: DEADLINE_EXCEEDED
+*/
+exports.activateBackgroundEffect = functions.firestore
+  .document('/users/{userId}/backgroundEffectsCollection/effectId')
+  .onUpdate((change, context) => {
+    const newValue = change.after.data();
+    const previousData = change.before.data();
+    const userId = context.params.userId;
+    if (newValue.activated === previousData.activated) return null;
+    if (newValue.activated === true) {
+      const payload = JSON.parse(JSON.stringify(newValue));
+      delete payload.activated;
+      return admin.firestore().doc(`/users/${userId}`).update({
+        settings: {
+          activeBackgroundEffect: payload
+        }
+      })
+      .then((result) => {
+        console.log('Success', result)
+        return result;
+      })
+      .catch((e) => {
+        console.log('Error occurred', e)
+      });
+    }
+    return null;
+  });
